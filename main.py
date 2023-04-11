875f17b72d407619698cd48322cdde5568f1ff71
sagarvincent <sagarvincnt@gmail.com>
2023-04-11 14:29:52 -0700

    feature added : capable of iterating through images without getting stuck on recommendations and clicking on required images

--- main.py
+++ main.py
@@ -24,16 +24,55 @@ a = input("waiting for user load maximum data and press enter")
 #scrolling all the way up
 driver.execute_script("window.scrollTo(0,0);")
 
-
 #parsers the text as html code
 page_html = driver.page_source
 soup = BeautifulSoup(page_html, "html.parser")
 
 #gets all the html containers matching the given attributes
 containers = soup.find_all('div',attrs={"class":"isv-r PNCib MSM1fd BUooTd"})
-print(len(containers))
 
-# close the browser after process
-driver.quit()
+#iterate through the images by iterating through the result set
+i = 1
+j = 1
+k = 0
+for tag in containers:
+    k = k + 1
+    while(j<=k):
+        #get xpath of each html tag
+        xpath = f'/html/body/div[2]/c-wiz/div[3]/div[1]/div/div/div/div/div[1]/div[1]/span/div[1]/div[1]/div[{i}]/a[1]/div[1]/img'
+        try:
+
+            #check if the xpath points to correct image
+            tagi = str(driver.find_element(By.XPATH, xpath))
+
+            #click on the image
+            image = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, xpath)))
+            image.click()
+            #WebDriverWait(driver,20)
+
+            #imagepath = f"/html/body/div[2]/c-wiz/div[3]/div[2]/div[3]/div[2]/div/div[2]/div[2]/div[2]/c-wiz/div/div[2]/div[1]/a/img[1]"
+            #image_tag = str(driver.find_element(By.XPATH, imagepath))
+            #image_tag.getattribute('src')
+
+        except:
+            
+            print(i)
+            i = i + 1
+            
+            if j == len(containers)-1:
+                # close the browser after process
+                print(j,k)
+                driver.quit()
+                break
+
+            continue
+
+        
+        print(f'{j}th image')
+        i = i + 1
+        j = j + 1
+
+        
+
 
 
