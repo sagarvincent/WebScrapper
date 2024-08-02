00a528b18e9e4364e9959e5b35f7c9313c085412
sagarvincent <sagarvincent@gmail.com>
2024-08-02 16:31:28 +0100

    Have to develop logic for parsing html syntax

--- src/cparser.py
+++ src/cparser.py
@@ -1,11 +1,10 @@
-import requests
-from bs4 import BeautifulSoup
+import subprocess
 
+def file_to_parse():
+    with open('file_path') as file:
+        html_content = file.read()
+    # create a sub process
+    cpp_parser = subprocess.Popen(['./parser'],stdin=subprocess.PIPE,stdout=subprocess.PIPE,text=True)
+    output,error = cpp_parser.communicate(input=html_content)
 
-
-class parser():
-    def __init__(self) -> None:
-        pass
-
-    def get_url():
-        pass
+    return output
--- src/parser.cpp
+++ src/parser.cpp
@@ -1,17 +1,33 @@
-#include <iostream>
-#include <fstream>
-#include <string>
+#include <stdio.h>
+#include<stack>
+#include<string>
+#include<queue>
 
-std::string main() {
-    std::ifstream file("filename.html");
-    std::string line;
+using namespace std;
 
-    while(std::getline(file,line)) {
-        if(line.find("<") !=std::string::npos) {
-            size_t start = line.find("<")+1;
-            size_t end = line.find(">");
-            std::string tag = line.substr(start, end - start);
+std::string parser(std::string html_content) 
+{
+    int i = 0;
+    stack<std::string> elem_end;
+    queue<std::string> elem_start;
+    bool is_elem;
+    stack<std::string> tag_end;
+    queue<std::string> tag_start;
+    bool is_tag;
+    char current;    
+    while(i<=html_content.size())
+    {
+        current = html_content[0];
+        if(current == '<') 
+        {
+            tag_start.push(current);
+            is_tag = true;
 
         }
-    }
+        else if(current == '>')
+        {
+            tag_end.push(current);
+            is_tag = false;
+        }
+    } 
 }
